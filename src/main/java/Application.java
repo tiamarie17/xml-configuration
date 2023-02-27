ba920118ba99c6056a420009a6a73af40022ae20
Tia Theisen <Tia.Theisen@state.mn.us>
2023-02-27 17:18:09 -0600

    add autowiring to applicationContext.xml and add code comments

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,4 @@
 import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,7 +6,7 @@ public class Application {
     public static void main(String args[]){
         ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-//        SpeakerService service = new SpeakerServiceImpl();
+// SpeakerService service = new SpeakerServiceImpl();
        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
